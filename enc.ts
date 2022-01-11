d7b9b3350a9ece80ba1448ea25cce580204e8d84
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2022-01-11 12:51:36 -0300

    Update enc.ts

--- enc.ts
+++ enc.ts
@@ -1,12 +1,12 @@
-var print : any = (what) => console.log(what);
+var print : any = (what: string) => console.log(what);
 
-var reverse = (what) => what.split("").reverse().join("");
+var reverse = (what : string) => what.split("").reverse().join("");
 
 var barista = {
   str1 : 'ion',
   str2 : reverse('rcne'),
   str3 : 'ypt',
-  request: function(preference) {
+  request: function(preference : string) {
     return preference + ' secret word: ' + this.str2 + this.str3 + this.str1;
   }
 }
